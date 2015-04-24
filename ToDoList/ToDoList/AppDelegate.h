289e38357c36a6db6091b44c477e809e43b65894
Casey <casey.j.wilkinson@gmail.com>
2015-04-25 00:56:44 +1000

    First commit of second attempt
    One step at a time…slowly…

---
+++ ToDoList/ToDoList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToDoList
+//
+//  Created by Casey Macrae on 25/04/2015.
+//  Copyright (c) 2015 Casey Macrae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ToDoList/ToDoList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ToDoList
+//
+//  Created by Casey Macrae on 25/04/2015.
+//  Copyright (c) 2015 Casey Macrae. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
